cd1d606f448712ccc0c9290050e2ef23113db7aa
Aaron Christianson <ninjaaron@gmail.com>
2016-08-03 19:19:27 -0500

    fixed url in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1',
     py_modules=['fastentrypoints'],
     long_description=open('README.rst').read(),
-    url='https://github.com/ninjaaron/fastentrypoints',
+    url='https://github.com/ninjaaron/fast-entry_points',
     author='Aaron Christianson',
     author_email='ninjaaron@gmail.com',
     license='BSD',
