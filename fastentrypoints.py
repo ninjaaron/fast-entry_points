2ec092784cccd7f647c2f237467d332e8c398c5a
Tomas Babej <tomasbabej@gmail.com>
2016-11-30 13:59:18 +0100

    Insert the import line to the setup.py

--- fastentrypoints.py
+++ fastentrypoints.py
@@ -44,9 +44,19 @@ def main():
     for dst in dests:
         shutil.copy(filename, dst)
         manifest_path = os.path.join(dst, 'MANIFEST.in')
+        setup_path = os.path.join(dst, 'setup.py')
 
         # Insert the include statement to MANIFEST.in if not present
         with open(manifest_path, 'a+') as manifest:
             manifest.seek(0)
             if not 'include fastentrypoints.py' in manifest.read():
                 manifest.write('\ninclude fastentrypoints.py')
+
+        # Insert the import statement to setup.py if not present
+        with open(setup_path, 'a+') as setup:
+            setup.seek(0)
+            setup_content = setup.read()
+            if not 'import fastentrypoints' in setup_content:
+                setup.seek(0)
+                setup.truncate()
+                setup.write('import fastentrypoints\n' + setup_content)
