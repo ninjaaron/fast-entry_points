4f6356eec450633bf84c8eee97bcdf23ea766d2c
Aaron Christianson <ninjaaron@gmail.com>
2017-02-19 10:37:37 +0100

    copy the script from wheel

--- fastentrypoints.py
+++ fastentrypoints.py
@@ -7,6 +7,16 @@ Monkey patch setuptools to write faster console_scripts with this format:
 This is better.
 '''
 from setuptools.command import easy_install
+TEMPLATE = '''\
+# -*- coding: utf-8 -*-
+import re
+import sys
+
+from {0} import {1}
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit({1}()) '''
 
 
 @classmethod
@@ -15,7 +25,7 @@ def get_args(cls, dist, header=None):
     Yield write_script() argument tuples for a distribution's
     console_scripts and gui_scripts entry points.
     """
-    template = 'import sys\nfrom {0} import {1}\nsys.exit({1}())'
+    # template = 'import sys\nfrom {0} import {1}\nsys.exit({1}())'
     if header is None:
         header = cls.get_header()
     spec = str(dist.as_requirement())
@@ -23,7 +33,7 @@ def get_args(cls, dist, header=None):
         group = type_ + '_scripts'
         for name, ep in dist.get_entry_map(group).items():
             cls._ensure_safe_name(name)
-            script_text = template.format(
+            script_text = TEMPLATE.format(
                           ep.module_name, ep.attrs[0])
             args = cls._get_script_args(type_, name, header, script_text)
             for res in args:
