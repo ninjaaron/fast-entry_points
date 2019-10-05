ee8e549209197eceb7b2f73489e5a5e1ca6c6b7d
Aaron Christianson <ninjaaron@gmail.com>
2019-10-05 11:15:29 +0200

    make setup script 2.7 compatible

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 import fastentrypoints
+import io
 
 setup(
     name='fastentrypoints',
     version='0.12',
     py_modules=['fastentrypoints'],
     description='Makes entry_points specified in setup.py load more quickly',
-    long_description=open('README.rst', 'r', encoding='utf-8').read(),
+    long_description=io.open('README.rst', 'r', encoding='utf-8').read(),
     url='https://github.com/ninjaaron/fast-entry_points',
     author='Aaron Christianson',
     author_email='ninjaaron@gmail.com',
